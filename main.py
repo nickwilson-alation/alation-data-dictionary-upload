e45e4cd5004b820935827e4e1d4d7c646ef07860
Nick Wilson <nicholas.wilson@alation.com>
2023-09-29 16:10:24 -0600

    Other enhancements

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ def main():
     def upload_csv(base_url, object_type, object_id, filename, token, overwrite_values):
         file_path = os.path.join("csv_upload_files", filename)  # Construct file path from filename
         url = f"https://{base_url}/integration/v1/data_dictionary/{object_type}/{object_id}/upload/"
-        headers = {"Authorization": f"Token {token}"}
+        headers = {"TOKEN": token}
 
         try:
             with open(file_path, "rb") as file:
-                files = {"file": (filename, file)}  # Using filename directly here
+                print(f"Uploading file: {file_path}")  # Printing the file path being used
+                print(f"File type: {'text/csv'}")  # Printing the file type
+                
+                files = {"file": (filename, file, 'text/csv')}  # Explicitly setting the content type to text/csv
                 params = {"overwrite_values": overwrite_values}
                 response = requests.put(url, headers=headers, files=files, params=params)
 
-            response.raise_for_status()  
+            response.raise_for_status()
             job = response.json()
             print("Upload job created:", job)
-            return job['id']
+            return job['task']['id'] 
         except requests.HTTPError as err:
             print(f"HTTP error occurred: {err}")
+            print(f"Response text: {err.response.text}")  # This will provide more detail on the error
         except Exception as err:
             print(f"An error occurred: {err}")
             sys.exit(1)
 
     def check_job_status(base_url, task_id, token):
-        url = f"https://{base_url}/integration/v1/data_dictionary/tasks/{task_id}/"
-        headers = {"Authorization": f"Token {token}"}
+        url = f"https://{base_url}/integration/v1/data_dictionary/tasks/{task_id}"
+        headers = {"TOKEN": token}
         
         try:
             while True:
