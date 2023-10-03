615ec89ce840887a95f901460f0632e909d92630
Nick Wilson <nicholas.wilson@alation.com>
2023-10-03 12:00:47 -0600

    Bit of a refactor

--- main.py
+++ main.py
@@ -6,57 +6,58 @@ import argparse
 import pandas as pd
 from html import escape
 
+# Sanitizes HTML to work around a python-magic library issue on the backend
 def sanitize_html(text):
     if pd.isna(text):
         return text
     return escape(text)
 
+def upload_csv(base_url, object_type, object_id, filename, token, overwrite_values):
+    file_path = os.path.join("csv_upload_files", filename)  # Construct file path from filename
+    url = f"https://{base_url}/integration/v1/data_dictionary/{object_type}/{object_id}/upload/"
+    headers = {"TOKEN": token}
+
+    try:
+        with open(file_path, "rb") as file:
+            files = {"file": (filename, file)}
+            params = {"overwrite_values": overwrite_values}
+            response = requests.put(url, headers=headers, files=files, params=params)
+        response.raise_for_status()
+        job = response.json()
+        print("Upload job created:", job)
+        return job['task']['id'] 
+    except requests.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+        print(f"Response text: {err.response.text}")  # This will provide more detail on the error
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        sys.exit(1)
+
+def check_job_status(base_url, task_id, token):
+    url = f"https://{base_url}/integration/v1/data_dictionary/tasks/{task_id}"
+    headers = {"TOKEN": token}
+    
+    try:
+        while True:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+
+            job_status = response.json()
+            print("Job Status:", job_status['state'], "-", job_status['status'] if 'status' in job_status else "")
+            
+            if job_status['state'] == "COMPLETED":
+                print("Job completed.")
+                return job_status
+            elif job_status['state'] == "PROCESSING":
+                print("Progress:", job_status['progress']['number_of_batches_completed'], "/", job_status['progress']['total_number_of_batches'])
+                time.sleep(5)
+    except requests.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        sys.exit(1)
+
 def main():
-    def upload_csv(base_url, object_type, object_id, filename, token, overwrite_values):
-        file_path = os.path.join("csv_upload_files", filename)  # Construct file path from filename
-        url = f"https://{base_url}/integration/v1/data_dictionary/{object_type}/{object_id}/upload/"
-        headers = {"TOKEN": token}
-
-        try:
-            with open(file_path, "rb") as file:
-                files = {"file": (filename, file)}
-                params = {"overwrite_values": overwrite_values}
-                response = requests.put(url, headers=headers, files=files, params=params)
-            response.raise_for_status()
-            job = response.json()
-            print("Upload job created:", job)
-            return job['task']['id'] 
-        except requests.HTTPError as err:
-            print(f"HTTP error occurred: {err}")
-            print(f"Response text: {err.response.text}")  # This will provide more detail on the error
-        except Exception as err:
-            print(f"An error occurred: {err}")
-            sys.exit(1)
-
-    def check_job_status(base_url, task_id, token):
-        url = f"https://{base_url}/integration/v1/data_dictionary/tasks/{task_id}"
-        headers = {"TOKEN": token}
-        
-        try:
-            while True:
-                response = requests.get(url, headers=headers)
-                response.raise_for_status()  # Raise an exception for HTTP errors
-
-                job_status = response.json()
-                print("Job Status:", job_status['state'], "-", job_status['status'] if 'status' in job_status else "")
-                
-                if job_status['state'] == "COMPLETED":
-                    print("Job completed.")
-                    return job_status
-                elif job_status['state'] == "PROCESSING":
-                    print("Progress:", job_status['progress']['number_of_batches_completed'], "/", job_status['progress']['total_number_of_batches'])
-                    time.sleep(5)
-        except requests.HTTPError as err:
-            print(f"HTTP error occurred: {err}")
-        except Exception as err:
-            print(f"An error occurred: {err}")
-            sys.exit(1)
-
     parser = argparse.ArgumentParser(description="Upload CSV to Alation Data Dictionary")
     parser.add_argument("--filename", required=True, help="Filename of the CSV")
     parser.add_argument("--base-url", required=True, help="Alation base URL")
